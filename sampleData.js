5c608a2759da0d1a47a3ddbaa2e2cd60e297bc5d
Tanzhi Liu <winohbell@yahoo.com>
2020-09-30 01:05:25 -0400

    finish createTable

---
+++ sampleData.js
@@ -0,0 +1,55 @@
+let item = [
+  {
+    itemID: 1,
+    handMade: true,
+    materials: " ",
+    description: " ",
+    sellerID: 12345
+  },
+  {
+    itemID: 2,
+    handMade: true,
+    materials: " ",
+    description: " ",
+    sellerID: 12345
+  },
+  {
+    itemID: 100,
+    handMade: true,
+    materials: " ",
+    description: " ",
+    sellerID: 12321
+  }
+]
+
+
+let seller = [
+  {
+    sellerID: 12345,
+    sellerName: " ",
+    storeName: " ",
+    shippingSpeedFastest: 3,
+    shippingSpeedSlowest: 5,
+    sellerLocationState: " ",
+    sellerLocationCity: " ",
+    exchangeP: true,
+    exchangeInfo: " ",
+    returnCancelP: false,
+    returnCancelInfo: " ",
+    noReturnItems: " ",
+    conditionOfReturn: " 
+  },
+]
+
+FAQs: [
+  {
+    faqID: 1,
+    question: " ",
+    answer: " "
+  },
+  {
+    faqID: 2,
+    question: " ",
+    answer: " "
+  }
+],
